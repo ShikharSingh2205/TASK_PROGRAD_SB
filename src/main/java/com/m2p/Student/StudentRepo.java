8da6bec0e330b7859724e9f0b9e7a72bbaadeb79
ShikharSingh2205 <91791888+ShikharSingh2205@users.noreply.github.com>
2023-06-21 11:30:20 +0530

    Update StudentRepo.java

--- src/main/java/com/m2p/Student/StudentRepo.java
+++ src/main/java/com/m2p/Student/StudentRepo.java
@@ -1,28 +1,50 @@
 package com.m2p.Student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class StudentRepo {
-    List<Student> studentList = new ArrayList<>();
 
-    public List<Student> getStudentList(Integer id, String name) {
-//        System.out.println(id);
-//        System.out.println(name);
-        return studentList;
+    private List<Student> studentList = new ArrayList<>();
+
+    public List<Student> getStudentList() {
+        return this.studentList;
     }
 
-    public void saves(Student student) {
-//        System.out.println(student);
-        studentList.add(student);
-//        System.out.println(studentList);
+    public Student saves(Student student) {
+
+        Student isStudentAlreadyExist = this.getStudentById(student.getId());
+
+        // checking if the student is already exist.....
+        if(isStudentAlreadyExist != null) return null;
+
+        // saving the student....
+        this.studentList.add(student);
+
+        // returning the student...
+        return student;
     }
 
-    public void getStudentById(Integer id) {
-//        System.out.println(studentList);
+    public Student getStudentById(Integer id) {
 
+        // getting the size of the list...........
+        int n = studentList.size();
 
+        // Iterating over the loop to check whether any student exist with sae id.
+        for(int student = 0; student < n; student++) {
+            Student currentStudent = studentList.get(student);
+
+            if(currentStudent.getId() == id) {
+                // returning the currentStudent...
+                return currentStudent;
+            }
+        }
+        // Means NO Student found with id as id.
+        return null;
     }
 }
